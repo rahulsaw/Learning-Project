d4efb8c102ad6be9d42c4908e6317fad206c0af1
rahulsaw12 <rahul.sawaria@housing.com>
2021-05-13 12:51:57 +0530

    initial commit

--- src/main/java/com/learn/learningproject/config/SwaggerConfig.java
+++ src/main/java/com/learn/learningproject/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.learn.learningproject;
+package com.learn.learningproject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/learn/learningproject/controller/TestController.java
+++ src/main/java/com/learn/learningproject/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.learn.learningproject;
+package com.learn.learningproject.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
