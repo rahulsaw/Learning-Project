fc79e2c7d88fb54fdbe78da4c2bb2a68ea884cc5
rahulsaw12 <rahul.sawaria@housing.com>
2021-05-13 12:37:30 +0530

    Initial commit

---
+++ src/main/java/com/learn/learningproject/ServletInitializer.java
@@ -0,0 +1,19 @@
+package com.learn.learningproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServletInitializer.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ServletInitializer.class);
+    }
+}
---
+++ src/main/java/com/learn/learningproject/TestController.java
@@ -0,0 +1,28 @@
+package com.learn.learningproject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/*
+Rahul sawaria created on 11/05/21
+*/
+@RestController
+public class TestController {
+
+    public final static Logger log = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("/hello")
+    public Collection<String> sayHello() {
+        log.info("inside the test controller say hello method..");
+        return IntStream.range(0, 10)
+                .mapToObj(i -> "Hello number " + i).collect(Collectors.toList());
+
+
+    }
+}
